39610412a1558cf4d0ad819492eb06a076da2c71
Leonid <rtdddawda2534@yandex.ru>
2024-03-31 16:10:35 +0500

    test

--- GameWindow.py
+++ GameWindow.py
@@ -24,8 +24,8 @@ class GameWindow(QWidget):
     def game_prestart(self):
         pass
 
-    def game_loop(self):
-        self.update()
+    # def game_loop(self):
+    #     self.update()
 
     def paintEvent(self, event):
         painter = QPainter(self)
@@ -34,8 +34,22 @@ class GameWindow(QWidget):
         # draw starship
         painter.setPen(QPen(Qt.white, 3, Qt.SolidLine))
         c1, c2, c3, c4, c5 = self.starship_cords
+        print("")
         print(c1, c2, c3, c4, c5)
         painter.drawLine(*c1, *c2)
         painter.drawLine(*c1, *c3)
         painter.drawLine(*c4, *c5)
         painter.end()
+
+    def keyPressEvent(self, event):
+        if event.key() == Qt.Key_Left:
+            print("bebra")
+            self.starship.rotate(10)
+            self.starship.calc_cords()
+            # self.update()
+
+    def on_rotate_left(self):
+        print("bebra")
+        self.starship.rotate(10)
+        self.starship_cords = self.starship.calc_cords()
+        self.update()
--- Objects/Starship.py
+++ Objects/Starship.py
@@ -6,7 +6,8 @@ class Starship(MoveableObject):
     def __init__(self, w, h):
         self.x = w // 2
         self.y = h // 2
-        self.angle = 40 / 180 * pi
+        self.angle = 40 / 180 * pi  # в градусах
+        self.angle_rotation = 0  # в градусах
         self.side = 70
         self.speed = [0, 0]
         self.a = [0, 0]
@@ -20,7 +21,19 @@ class Starship(MoveableObject):
         c0 = int(self.x), int(self.y)
         c1 = (int(self.x - self.side * sin(self.angle / 2)), int(self.y + self.side * cos(self.angle / 2)))
         c2 = (int(self.x + self.side * sin(self.angle / 2)), int(self.y + self.side * cos(self.angle / 2)))
-        c3 = (int((self.x - self.side * 0.8 * sin(self.angle / 2))), int((self.y + self.side * 0.8 * cos(self.angle / 2))))
-        c4 = (int((self.x + self.side * 0.8 * sin(self.angle / 2))), int((self.y + self.side * 0.8 * cos(self.angle / 2))))
+        c3 = (
+            int((self.x - self.side * 0.8 * sin(self.angle / 2))),
+            int((self.y + self.side * 0.8 * cos(self.angle / 2))))
+        c4 = (
+            int((self.x + self.side * 0.8 * sin(self.angle / 2))),
+            int((self.y + self.side * 0.8 * cos(self.angle / 2))))
 
         return c0, c1, c2, c3, c4
+
+    def rotate(self, angle):
+        angle = (angle+90) / 180 * pi
+        r = self.side * cos(self.angle / 2)
+        self.x = int(self.x + r * cos(angle))
+        self.y = int(self.y + r * sin(angle))
+        self.angle_rotation += angle
+        print(self.x, self.y)
