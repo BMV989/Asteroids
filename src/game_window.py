a2d8b4daff80bf5735999ecb46b2ee488370353a
Maxim <mbrozovskii@gmail.com>
2024-04-05 20:05:51 +0500

    almost mvp v0.1 with good things from develop

--- src/game_window.py
+++ src/game_window.py
@@ -1,8 +1,9 @@
-from PyQt5.QtCore import Qt, QTimer
+from PyQt5.QtCore import Qt, QTimer, QPoint
 from PyQt5.QtGui import QImage, QPainter, QPen
 from PyQt5.QtWidgets import QWidget
 
 from src.entities.asteroid import Asteroid
+from src.entities.bullet import Bullet
 from src.entities.starship import Starship
 
 
@@ -12,7 +13,7 @@ class GameWindow(QWidget):
 
         self.width = width
         self.height = height
-
+        self.bullets = []
         self.resize(self.width, self.height)
 
         self.starship = Starship()
@@ -23,6 +24,9 @@ class GameWindow(QWidget):
     def game_loop(self):
         for aster in self.asteroids:
             aster.update_with_bounds()
+        self.starship.upd()
+        for bullet in self.bullets:
+            bullet.upd()
         self.update()
 
     def paintEvent(self, event):
@@ -34,3 +38,31 @@ class GameWindow(QWidget):
         self.starship.paint(painter)
         for aster in self.asteroids:
             aster.paint(painter)
+        for bullet in self.bullets:
+            bullet.paint(painter)
+
+    def shoot(self):
+        self.bullets.append(
+            Bullet(QPoint(self.starship.side_x0, self.starship.side_y0), self.starship.angle_rotation))
+
+    def on_move(self):
+        self.starship.fast_forward_mode = True
+
+    def stop_move(self):
+        self.starship.fast_forward_mode = False
+
+    def on_rotate_left(self):
+        self.starship.rotate_left = True
+
+    def stop_rotate_left(self):
+        self.starship.rotate_left = False
+
+    def on_rotate_right(self):
+        self.starship.rotate_right = True
+
+    def stop_rotate_right(self):
+        self.starship.rotate_right = False
+
+    def on_move_forward(self):
+        self.starship.move()
+        self.starship.calc_all_cords()
